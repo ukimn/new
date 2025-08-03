b6496e8bf5f309d2821936f2d699e272d62bc8e5
as <ya@gmail.com>
2025-08-04 02:51:44 +0300

    asdf

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const heading = document.querySelector('.heading');
 
 const button = document.querySelector('.root');
 
+const list = ["I love You!", "You're the Best", "Your're Handsom", "You're Wonderful"]
+const randomList = Math.trunc(Math.random() * list.length)
+
 button.addEventListener('click', ()=>{
-    heading.textContent = "I love You!";
+    heading.textContent = list[randomList];
 })
