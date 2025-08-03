bb51ceeb147604e49e7fac911b9cda62267e9e68
as <ya@gmail.com>
2025-08-04 02:52:16 +0300

    fads

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const heading = document.querySelector('.heading');
 
 const button = document.querySelector('.root');
 
-const list = ["I love You!", "You're the Best", "Your're Handsom", "You're Wonderful"]
+const list = ["I love You!", "You're the Best", "Your're Handsom", "You're Wonderful", "You're too bad", "You're stupid", "You're hungry"]
 const randomList = Math.trunc(Math.random() * list.length)
 
 button.addEventListener('click', ()=>{
